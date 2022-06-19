479ba11a842f1912915a0c9236334e4a2938837d
Zach <54410451+zach2y@users.noreply.github.com>
2022-06-20 00:31:04 +0800

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,48 @@
+# Load libraries
+library(dplyr)
+library(data.table)
+
+# Read files
+features <- fread("./UCI HAR Dataset/features.txt", col.names = c("index","featureName"))
+uniqueFeatures <- make.unique(features$featureName, sep="_")
+activityLabels <- fread("./UCI HAR Dataset/activity_labels.txt", col.names = c("index","activityLabel"))
+testData <- fread("./UCI HAR Dataset/test/X_test.txt", col.names = uniqueFeatures)
+testLabel <- fread("./UCI HAR Dataset/test/y_test.txt", col.names = "activity")
+testSubject <- fread("./UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+trainData <- fread("./UCI HAR Dataset/train/X_train.txt", col.names = uniqueFeatures)
+trainLabel <- fread("./UCI HAR Dataset/train/y_train.txt", col.names = "activity")
+trainSubject <- fread("./UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+
+# 1. Merges the training and the test sets to create one data set.
+
+# Combine tables
+testData <- cbind(testSubject, testLabel, testData)
+trainData <- cbind(trainSubject, trainLabel, trainData)
+df <- rbind(testData,trainData)
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+df <- select(df, subject,activity, grep(".*mean.*|.*std.*", names(df), ignore.case=TRUE,value=TRUE))
+
+# 3. Uses descriptive activity names to name the activities in the data set
+df$activity <- activityLabels[df$activity, 2]
+
+# 4. Appropriately labels the data set with descriptive variable names. 
+names(df)<-gsub("^t", "time", names(df))
+names(df)<-gsub("^f", "frequency", names(df))
+names(df)<-gsub("Acc", "Accelerometer", names(df))
+names(df)<-gsub("Gyro", "Gyroscope", names(df))
+names(df)<-gsub("BodyBody", "Body", names(df))
+names(df)<-gsub("Mag", "Magnitude", names(df))
+names(df)<-gsub("tBody", "TimeBody", names(df))
+names(df)<-gsub("-mean", "Mean", names(df), ignore.case = TRUE)
+names(df)<-gsub("-std", "STD", names(df), ignore.case = TRUE)
+names(df)<-gsub("-freq", "Frequency", names(df), ignore.case = TRUE)
+names(df)<-gsub("\\(\\)", "", names(df))
+names(df)<-gsub("\\-", "", names(df))
+ 
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each 
+#    variable for each activity and each subject.
+tidydf <- as_tibble(df)
+tidyMeans <- tidydf %>% group_by(subject, activity) %>% 
+        summarize(across(everything(), list(mean = ~ mean(.x, na.rm = TRUE))))
+write.table(tidyMeans, "tidyData.txt", row.name=FALSE)
